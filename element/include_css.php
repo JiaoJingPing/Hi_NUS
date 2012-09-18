918dedb154b820040a123b6bab1ed1fcae23b21f
Han Yu <moonlight612311521@gmail.com>
2012-09-19 03:24:59 +0800

    edit profile page

--- element/include_css.php
+++ element/include_css.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../util.php';
 <!--<link rel="shortcut icon" href="<?php echo get_file_url('img/favicon.ico'); ?>" />-->
 <link rel="stylesheet" href="<?php echo get_file_url('css/jquery.mobile-1.1.1.min.css'); ?>" />
 <link rel="stylesheet" href="<?php echo get_file_url('css/custom.css'); ?>" />
-
+<link rel="stylesheet" href="<?php echo get_file_url('css/profile.css'); ?>" />
--- element/profile.php
+++ element/profile.php
@@ -1,4 +1,16 @@
 <!-- My profile -->
+
+<style type= "text/css">
+	grid-a > :nth-child(n) {
+		width: 30%;
+		margin-right: -.5px;
+	}
+
+	#photo-ceil{
+		height:120px; 
+		padding-top:55px;
+	}
+</style>
 		<div data-role="page" id="page6">
 		    <div data-theme="a" data-role="header">
 		        <a data-role="button" data-transition="fade" href="#page6" class="ui-btn-right">
@@ -8,77 +20,86 @@
 		            Profile
 		        </h3>
 		    </div>
-		    <div data-role="content" class="middlecontent">
+		    <div data-role="content" class="middlecontent ceil-text">
 		        <div class="ui-grid-a">
-		            <div class="ui-block-a">
+		            <div class="ui-block-a ceil-text-a" id="photo-ceil">
 		                <b>Photo</b>
 		            </div>
-		            <div class="ui-block-b">
-		            	<div style="">
+		            <div class="ui-block-b ceil-text-b">
+		            	<div>
 		                    <img style="width: 150px; height: 120px" src="http://assets.codiqa.com/ps/33214/5f4d06279d261a441-1111051TA1.jpg">
+		                	<button data-inline="true" data-theme="b">Change photo</button>
 		                </div>
 		            </div>
-		            <div class="ui-block-a">
+		            <div class="ui-block-a ceil-text-a">
+		            	<div>
+		            		<p>
+		            			<b>Last Location</b>
+		            		</p>
+		            	</div>
+		            </div>
+		            <div class="ui-block-b ceil-text-b">
+		            	<div>
+		            		<p>
+		            			School of Computing
+		            		</p>
+		            	</div>
+		            </div>
+		            <div class="ui-block-a ceil-text-a">
 		                <div>
 		                    <p>
 		                        <b>Name</b>
 		                    </p>
 		                </div>
 		            </div>
-		            <div class="ui-block-b">
+		            <div class="ui-block-b ceil-text-b">
 		                <div>
 		                    <p>
-		                        <b>Colin Tan</b>
+		                        Colin Tan
 		                    </p>
 		                </div>
 		            </div>
-		            <div class="ui-block-a">
+		            <div class="ui-block-a ceil-text-a">
 		                <div>
 		                    <p>
 		                        <b>Gender</b>
 		                    </p>
 		                </div>
 		            </div>
-		            <div class="ui-block-b">
+		            <div class="ui-block-b ceil-text-b">
 		                <div>
 		                    <p>
-		                        <b>male</b>
+		                        male
 		                    </p>
 		                </div>
 		            </div>
-		            <div class="ui-block-a">
+		            <div class="ui-block-a ceil-text-a">
+		                <div>
+		                    <p>
+		                        <b>Hobbies</b>
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="ui-block-b ceil-text-b">
+		                <div>
+		                    <p>
+		                        I like computing hahaha
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="ui-block-a ceil-text-a">
 		                <div>
 		                    <p>
 		                        <b>Self-introduction</b>
 		                    </p>
 		                </div>
 		            </div>
-		            <div class="ui-block-b">
+		           	<div class="ui-block-b ceil-text-b">
 		                <div>
 		                    <p>
-		                        <b>I am very happy</b>
+		                        I am the lecture of cs3216
 		                    </p>
 		                </div>
-		            </div>
-		            <div class="ui-block-a">
-		                <div>
-		                    <p>
-		                        <b>Faculty</b>
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-b">
-		                <div>
-		                    <p>
-		                        <b>
-		                            I am the Lecture of cs3216 hahaha
-		                        </b>
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-a">
-		            </div>
-		            <div class="ui-block-b">
 		            </div>
 		        </div>
 		    </div>
