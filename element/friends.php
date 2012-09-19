e355f883d9854d62eff1393b82077ec9465f859d
Han Yu <moonlight612311521@gmail.com>
2012-09-19 18:28:47 +0800

    add friends page

--- element/friends.php
+++ element/friends.php
@@ -1,4 +1,35 @@
 <!-- Friends -->
+
+<?php
+	$friend_info = "";
+
+?>
+<style type="text/css">
+
+table{
+	width:100%;
+}
+td.left {
+	width:25%;
+}
+td.right {
+	height:100%;
+}
+td div#name{
+	height:33%;
+	margin-bottom:5px
+}
+
+td div#other{
+	color:grey;
+	height:33%;
+	margin-bottom:5px;
+}
+
+
+</style>
+
+
 <div data-role="page" id="page5">
 	<div data-theme="a" data-role="header">
 		<h3>
@@ -6,7 +37,30 @@
 		</h3>
 	</div>
 	<div data-role="content" class="middlecontent">
-	Content goes here
+		<ul id="friend-list" data-role="listview" data-filter="true" data-inset="true" data-filter-placeholder="Search people...">
+
+			<li><a href="index.html">
+				<div style="display:inline">
+					<table>
+						<tr>
+							<td class="left" >
+								<span ><img src="images/meinv.jpg" height="60" width="60" /></span>
+							</td>
+							<td class="right" >
+								<div id="name">Bei Chuan Jing Zi
+								</div>
+								<div id="other">10km | 3days ago
+								</div>
+								<div id="other">too young too simple
+								</div>
+							</td>
+
+						</tr>
+					</table>
+				</div>
+			</a></li>
+		</ul>
+
 	</div>
 	<div data-role="footer">
 		<div data-role="navbar" data-iconpos="left" data-theme="a">
--- element/include_css.php
+++ element/include_css.php
@@ -5,3 +5,4 @@ require_once __DIR__ . '/../util.php';
 <link rel="stylesheet" href="<?php echo get_file_url('css/jquery.mobile-1.1.1.min.css'); ?>" />
 <link rel="stylesheet" href="<?php echo get_file_url('css/custom.css'); ?>" />
 <link rel="stylesheet" href="<?php echo get_file_url('css/profile.css'); ?>" />
+<link rel="stylesheet" href="<?php echo get_file_url('css/friends.css'); ?>" />
