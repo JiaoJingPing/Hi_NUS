0c1f9818a66a27e36e529780ebf7e8183016426d
Han Yu <moonlight612311521@gmail.com>
2012-09-20 00:29:25 +0800

    new profile page much nicer

--- element/friends.php
+++ element/friends.php
@@ -2,6 +2,28 @@
 
 <?php
 	$friend_info = "";
+	$name = "Bei Chuan Jing Zi";
+	$distance = 10;
+	$last_active_time = 3;
+	$status = "too young too simple";
+	$friend_info.='
+	<li><a href="index.html" style="padding:5px">
+					<table>
+						<tr>
+							<td class="left" >
+								<span ><img src="images/meinv.jpg" height="70" width="70" /></span>
+							</td>
+							<td class="right" >
+								<div id="name">'.$name.'
+								</div>
+								<div id="other">'.$distance.'km | '.$last_active_time.'days ago
+								</div>
+								<div id="other">'.$status.'
+								</div>
+							</td>
+						</tr>
+					</table>
+			</a></li>';
 
 ?>
 <style type="text/css">
@@ -26,7 +48,6 @@ td div#other{
 	margin-bottom:5px;
 }
 
-
 </style>
 
 
@@ -38,27 +59,7 @@ td div#other{
 	</div>
 	<div data-role="content" class="middlecontent">
 		<ul id="friend-list" data-role="listview" data-filter="true" data-inset="true" data-filter-placeholder="Search people...">
-
-			<li><a href="index.html">
-				<div style="display:inline">
-					<table>
-						<tr>
-							<td class="left" >
-								<span ><img src="images/meinv.jpg" height="60" width="60" /></span>
-							</td>
-							<td class="right" >
-								<div id="name">Bei Chuan Jing Zi
-								</div>
-								<div id="other">10km | 3days ago
-								</div>
-								<div id="other">too young too simple
-								</div>
-							</td>
-
-						</tr>
-					</table>
-				</div>
-			</a></li>
+			<?php echo($friend_info); ?>
 		</ul>
 
 	</div>
--- element/profile.php
+++ element/profile.php
@@ -1,133 +1,85 @@
-<!-- My profile -->
+<?php
+	$name = "Bei Chuan";
+	$last_location ="National University of Singapore SOC";
+	$status = "CSS is really a shit stuff";
+	$gender="female";
+	$education="National University of Singapore SOC";
+	$hobbies ="Reading, Swimming, Playing games";
+?>
 
-<style type= "text/css">
-	grid-a > :nth-child(n) {
-		width: 30%;
-		margin-right: -.5px;
-	}
 
-	#photo-ceil{
-		height:120px; 
-		padding-top:55px;
-	}
-</style>
-		<div data-role="page" id="page6">
-		    <div data-theme="a" data-role="header">
-		        <a data-role="button" data-transition="fade" href="#page6" class="ui-btn-right">
-		            Edit
-		        </a>
-		        <h3>
-		            Profile
-		        </h3>
-		    </div>
-		    <div data-role="content" class="middlecontent ceil-text">
-		        <div class="ui-grid-a">
-		            <div class="ui-block-a ceil-text-a" id="photo-ceil">
-		                <b>Photo</b>
-		            </div>
-		            <div class="ui-block-b ceil-text-b">
-		            	<div>
-		                    <img style="width: 150px; height: 120px" src="http://assets.codiqa.com/ps/33214/5f4d06279d261a441-1111051TA1.jpg">
-		                	<button data-inline="true" data-theme="b">Change photo</button>
-		                </div>
-		            </div>
-		            <div class="ui-block-a ceil-text-a">
-		            	<div>
-		            		<p>
-		            			<b>Last Location</b>
-		            		</p>
-		            	</div>
-		            </div>
-		            <div class="ui-block-b ceil-text-b">
-		            	<div>
-		            		<p>
-		            			School of Computing
-		            		</p>
-		            	</div>
-		            </div>
-		            <div class="ui-block-a ceil-text-a">
-		                <div>
-		                    <p>
-		                        <b>Name</b>
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-b ceil-text-b">
-		                <div>
-		                    <p>
-		                        Colin Tan
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-a ceil-text-a">
-		                <div>
-		                    <p>
-		                        <b>Gender</b>
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-b ceil-text-b">
-		                <div>
-		                    <p>
-		                        male
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-a ceil-text-a">
-		                <div>
-		                    <p>
-		                        <b>Hobbies</b>
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-b ceil-text-b">
-		                <div>
-		                    <p>
-		                        I like computing hahaha
-		                    </p>
-		                </div>
-		            </div>
-		            <div class="ui-block-a ceil-text-a">
-		                <div>
-		                    <p>
-		                        <b>Self-introduction</b>
-		                    </p>
-		                </div>
-		            </div>
-		           	<div class="ui-block-b ceil-text-b">
-		                <div>
-		                    <p>
-		                        I am the lecture of cs3216
-		                    </p>
-		                </div>
-		            </div>
-		        </div>
-		    </div>
-		    <div data-role="footer">
-			    <div data-role="navbar" data-iconpos="left" data-theme="a">
-			        <ul>
-			            <li>
-			                <a href="#page1" data-transition="slide" data-direction="reverse"  data-theme="" data-icon="home">
-			                    Nearby
-			                </a>
-			            </li>
-			            <li>
-			                <a href="#page5" data-transition="slide" data-direction="reverse"  data-theme="" data-icon="plus">
-			                    Friends
-			                </a>
-			            </li>
-			            <li>
-			                <a data-theme="b" data-icon="info">
-			                    Profile
-			                </a>
-			            </li>
-			            <li>
-			                <a href="#page7" data-transition="slide" data-theme="" data-icon="gear">
-			                    Setting
-			                </a>
-			            </li>
-			        </ul>
-			    </div>
-			</div>
-		</div>
-		
+<div data-role="page" id="page6">
+    <div data-theme="a" data-role="header">
+        <a data-role="button" data-transition="fade" href="#page6" class="ui-btn-right" style="width:50px">
+            Edit
+        </a>
+        <h3>
+            Profile
+        </h3>
+    </div>
+    <div data-role="content" class="middlecontent ceil-text">
+    	<div class="photo-area">
+    		<img class="photo"  src="http://assets.codiqa.com/ps/33214/5f4d06279d261a441-1111051TA1.jpg">
+    		<!--<button id="change_photo" data-inline="true" data-theme="e">Change</button>-->
+    	</div>
+    	<div class="info-area">
+    		<ul id="info" data-role="listview" data-inset="true">
+    			<li class="single-line">
+    				<div class="single-line-left">Name</div>
+    				<div class="single-line-right <?php echo($gender);?>"><?php echo($name);?></div>
+    			</li>
+    			<li>
+    				<div>Last Location</div>
+    				<div class=<?php echo($gender);?>><?php echo($last_location);?></div>
+    			</li>
+    			<li>
+    				<div>Status</div>
+    				<div class=<?php echo($gender);?>><?php echo($status);?></div>
+    			</li>
+    		</ul>
+    		<br/>
+    		<ul data-role="listview" data-inset="true" data-divider-theme="a" >
+    			<li data-role="list-divider">Basic Info</li>
+    			<li class="single-line">
+    				<div class="single-line-left">Gender</div>
+    				<div class="single-line-right <?php echo($gender);?>"><?php echo(ucfirst($gender));?></div>
+    			</li>
+    			<li>
+    				<div>Education</div>
+    				<div class= <?php echo($gender);?> ><?php echo($education);?></div>
+    			</li>	        		
+    			<li>
+    				<div>Hobbies</div>
+    				<div class = <?php echo($gender);?> > <?php echo($hobbies);?></div>
+    			</li>
+    			
+    		</ul>
+        </div>
+    </div>
+    <div data-role="footer">
+	    <div data-role="navbar" data-iconpos="left" data-theme="a">
+	        <ul>
+	            <li>
+	                <a href="#page1" data-transition="slide" data-direction="reverse"  data-theme="" data-icon="home">
+	                    Nearby
+	                </a>
+	            </li>
+	            <li>
+	                <a href="#page5" data-transition="slide" data-direction="reverse"  data-theme="" data-icon="plus">
+	                    Friends
+	                </a>
+	            </li>
+	            <li>
+	                <a data-theme="b" data-icon="info">
+	                    Profile
+	                </a>
+	            </li>
+	            <li>
+	                <a href="#page7" data-transition="slide" data-theme="" data-icon="gear">
+	                    Setting
+	                </a>
+	            </li>
+	        </ul>
+	    </div>
+	</div>
+</div>
