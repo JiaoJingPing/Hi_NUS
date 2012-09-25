459fc0a4d0d762e2bb238e18ef49b21da538a911
Jiao Jing Ping <elleryjiao@gmail.com>
2012-09-25 11:53:20 +0800

    revise auth model

--- application/models/auth_model.php
+++ application/models/auth_model.php
@@ -1,44 +1,102 @@
 <?php
 class auth_model extends CI_Model {
-	const ADMIN_NAME = 'admin';
+	var $realm = 'The batcave';
 	function __construct() {
 		parent::__construct();
 		$this -> load -> model('user_model');
 	}
 
-	function isMember() {
-		if (!isset($_SERVER['PHP_AUTH_USER'])) {
-			header('WWW-Authenticate: Basic realm="server realm rocks"');
-			header('HTTP/1.0 401 Unauthorized');
-			return false;
-		} else {
-			return $this -> user_model -> authenticate($_SERVER['PHP_AUTH_USER'], md5($_SERVER['PHP_AUTH_PW']));
-		}
-	}
-
-	function isAdmin() {
-		if (!isset($_SERVER['PHP_AUTH_USER'])) {
-			header('WWW-Authenticate: Basic realm="server realm rocks"');
-			header('HTTP/1.0 401 Unauthorized');
-			return false;
-		} else if ($_SERVER['PHP_AUTH_USER'] == self::ADMIN_NAME) {
-			return $this -> user_model -> authenticate($_SERVER['PHP_AUTH_USER'], md5($_SERVER['PHP_AUTH_PW']));
-		} else {
-			return false;
-		}
-	}
-
 	function getCurrentUser() {
 		if ($this -> isMember()) {
-			return $_SERVER['PHP_AUTH_USER'];
+			$digest = $this -> getDigest();
+			$digestParts = $this -> digestParse($digest);
+			return $digestParts['username'];
 		} else {
 			return false;
 		}
 	}
 
-	function logout() {
-		Header('WWW-Authenticate: Basic realm="protected area"');
-		Header('HTTP/1.0 401 Unauthorized');
+	function login() {
+		
+
+	}
+
+	function isMember() {
+
+		$realm = $this -> realm;
+		$nonce = uniqid();
+
+		$digest = $this -> getDigest();
+
+		// If there was no digest, show login
+		if (is_null($digest))
+			$this -> requireLogin($realm, $nonce);
+
+		$digestParts = $this -> digestParse($digest);
+
+		if (!isset($digestParts['username'])) {
+			$this -> requireLogin($realm, $nonce);
+		}
+
+		$email = $digestParts['username'];
+
+		if (!$this -> user_model -> isExist(array('email' => $email))) {
+			$this -> requireLogin($realm, $nonce);
+		}
+
+		$validUser = $email;
+		$validPass = $this -> user_model -> getPassword($email);
+
+		// Based on all the info we gathered we can figure out what the response should be
+		$A1 = md5("{$validUser}:{$realm}:{$validPass}");
+		$A2 = md5("{$_SERVER['REQUEST_METHOD']}:{$digestParts['uri']}");
+
+		$validResponse = md5("{$A1}:{$digestParts['nonce']}:{$digestParts['nc']}:{$digestParts['cnonce']}:{$digestParts['qop']}:{$A2}");
+
+		if ($digestParts['response'] != $validResponse)
+			return false;
+
+		return TRUE;
+	}
+
+	function getDigest() {
+		$digest = null;
+		// mod_php
+		if (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+			$digest = $_SERVER['PHP_AUTH_DIGEST'];
+			// most other servers
+		} elseif (isset($_SERVER['HTTP_AUTHENTICATION'])) {
+
+			if (strpos(strtolower($_SERVER['HTTP_AUTHENTICATION']), 'digest') === 0)
+				$digest = substr($_SERVER['HTTP_AUTHORIZATION'], 7);
+		}
+
+		return $digest;
+
+	}
+
+	// This function forces a login prompt
+	function requireLogin($realm, $nonce) {
+		header('WWW-Authenticate: Digest realm="' . $realm . '",qop="auth",nonce="' . $nonce . '",opaque="' . md5($realm) . '"');
+		header('HTTP/1.0 401 Unauthorized');
+		echo 'Text to send if user hits Cancel button';
+		die();
+	}
+
+	// This function extracts the separate values from the digest string
+	function digestParse($digest) {
+		// protect against missing data
+		$needed_parts = array('nonce' => 1, 'nc' => 1, 'cnonce' => 1, 'qop' => 1, 'username' => 1, 'uri' => 1, 'response' => 1);
+		$data = array();
+
+		preg_match_all('@(\w+)=(?:(?:")([^"]+)"|([^\s,$]+))@', $digest, $matches, PREG_SET_ORDER);
+
+		foreach ($matches as $m) {
+			$data[$m[1]] = $m[2] ? $m[2] : $m[3];
+			unset($needed_parts[$m[1]]);
+		}
+
+		return $needed_parts ? false : $data;
 	}
 
 }
